4d60fd6397f03bb0f0aa426eb7aa164653b91866
xTrayambak <xtrayambak@gmail.com>
2023-12-24 12:15:17 +0530

    (fix) use ORC move semantics

--- src/libarpm/package.nim
+++ src/libarpm/package.nim
@@ -11,18 +11,44 @@ type
     Newer
     Same
 
-  PackageMetadata* = ref object
+  PackageMetadata* = object
     reason*: InstallationReason        ## Why was this package installed?
     freedom*: FreedomType              ## What freedom does this package give you?
 
-  Package* = ref object
+  Package* = object
     name*: string                      ## The package's name
     version*: Version                  ## The package's semantic version
     maintainer*: Maintainer            ## The package's maintainer on Argon
     license*: License                  ## The package's license
  
     depends*, provides*: seq[string]   ## What packages this depends upon and provides to
-    reason*: InstallationReason        ## Why was this package installed?
+    metadata*: PackageMetadata         ## Other information
+
+proc `=destroy`*(package: Package) =
+  `=destroy`(package.name)
+  `=destroy`(package.maintainer)
+  `=destroy`(package.depends)
+  `=destroy`(package.provides)
+
+proc `=sink`*(dest: var PackageMetadata, src: PackageMetadata) =
+  wasMoved(dest)
+  dest.reason = src.reason
+  dest.freedom = src.freedom
+
+proc `=sink`*(dest: var Package, src: Package) =
+  wasMoved(dest)
+
+  dest.name = src.name
+  dest.version = src.version
+  dest.maintainer = src.maintainer
+  dest.license = src.license
+
+  dest.depends = src.depends
+  dest.provides = src.provides
+  dest.metadata = src.metadata
+
+  `=sink`(dest.metadata, src.metadata)
+  `=destroy`(src)
 
 proc isLibre*(package: Package): bool {.inline.} =
   isLibre package.license
--- src/libarpm/parsers/maintainer.nim
+++ src/libarpm/parsers/maintainer.nim
@@ -3,16 +3,47 @@ import std/[strutils], ../io
 type
   ## Contact addresses for a maintainer.
   ## All of them are optional.
-  Contacts* = ref object
+  Contacts* = object
     github*, mastodon*, email*, gitlab*: string
   
   ## A maintainer's data.
-  Maintainer* = ref object
+  Maintainer* = object
     realName*: string                        ## The maintainer's real name
     contacts*: Contacts                      ## The maintainer's contacts
 
+proc `=destroy`*(contacts: Contacts) =
+  `=destroy`(contacts.github)
+  `=destroy`(contacts.mastodon)
+  `=destroy`(contacts.email)
+  `=destroy`(contacts.gitlab)
+
+proc `=sink`*(dest: var Contacts, src: Contacts) =
+  wasMoved(dest)
+
+  dest.github = src.github
+  dest.mastodon = src.mastodon
+  dest.email = src.email
+  dest.gitlab = src.gitlab
+
+  `=destroy`(src)
+
+proc `=destroy`*(maintainer: Maintainer) =
+  `=destroy`(maintainer.realName)
+  `=destroy`(maintainer.contacts)
+
+proc `=sink`*(dest: var Maintainer, src: Maintainer) =
+  wasMoved(dest)
+
+  dest.realName = src.realName
+  dest.contacts = src.contacts
+
+  `=destroy`(src)
+
 proc parseMaintainer*(mStr: string): Maintainer =
   ## Parse a maintainer from a Argon maintainer "spec-compliant" string.
+  ## <real name> (<contact_form_1>:<contact_address_1>,<contact_form_2>:<contact_address_2>) and so on, and so forth.
+  ##
+  ## Currently, only GitHub, Mastodon, Email and GitLab are supported.
   var 
     curr: char
     pos: int
