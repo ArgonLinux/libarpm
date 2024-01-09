a56a3a7d9589a8d06e3dd0cd8afe2ff8a997e888
xTrayambak <xtrayambak@gmail.com>
2024-01-09 15:02:31 +0530

    (add) add storage utility

---
+++ src/libarpm/storage.nim
@@ -0,0 +1,31 @@
+import std/[os, options], ./io, helpers
+
+const
+  BASE_STORAGE {.strdefine.} = "/etc/arpm.d"
+
+proc existsOrCreateStorageDir*(subdirectories: seq[string]) {.root.} =
+  if not dirExists("/etc/arpm.d"):
+    info "Creating storage directory (" & BASE_STORAGE & ")"
+    createDir(BASE_STORAGE)
+  
+  for sub in subdirectories:
+    if not dirExists(sub):
+      info "Creating sub-directory in storage directory (" & BASE_STORAGE & '/' & sub & ')'
+      createDir(BASE_STORAGE / sub)
+
+proc writePackageList*(name: string, data: string) {.inline, root.} =
+  existsOrCreateStorageDir(@["repos"])
+  writeFile(BASE_STORAGE / "repos" / name & ".argon", data)
+
+proc getPackageList*(
+  name: string
+): Option[string] =
+  info "Fetching pre-saved repository: " & name & ".argon"
+
+  let path = BASE_STORAGE / "repos" / name & ".argon"
+  
+  if not fileExists(path):
+    warn "Pre-saved repository not found: " & path
+    return none(string)
+  
+  some(readFile(path))
