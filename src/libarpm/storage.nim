5cfd1f3beaeb1e85d4064d09bd3efdb3c6310445
xTrayambak <xtrayambak@gmail.com>
2024-01-13 10:58:24 +0530

    (fix) root-checking bugs

--- src/libarpm/storage.nim
+++ src/libarpm/storage.nim
@@ -3,19 +3,21 @@ import std/[os, options], ./io, helpers
 const
   BASE_STORAGE {.strdefine.} = "/etc/arpm.d"
 
-proc existsOrCreateStorageDir*(subdirectories: seq[string]) {.root.} =
-  if not dirExists("/etc/arpm.d"):
-    info "Creating storage directory (" & BASE_STORAGE & ")"
-    createDir(BASE_STORAGE)
+proc existsOrCreateStorageDir*(subdirectories: seq[string]) =
+  root:
+    if not dirExists("/etc/arpm.d"):
+      info "Creating storage directory (" & BASE_STORAGE & ")"
+      createDir(BASE_STORAGE)
   
-  for sub in subdirectories:
-    if not dirExists(sub):
-      info "Creating sub-directory in storage directory (" & BASE_STORAGE & '/' & sub & ')'
-      createDir(BASE_STORAGE / sub)
+    for sub in subdirectories:
+      if not dirExists(sub):
+        info "Creating sub-directory in storage directory (" & BASE_STORAGE & '/' & sub & ')'
+        createDir(BASE_STORAGE / sub)
 
-proc writePackageList*(name: string, data: string) {.inline, root.} =
-  existsOrCreateStorageDir(@["repos"])
-  writeFile(BASE_STORAGE / "repos" / name & ".argon", data)
+proc writePackageList*(name: string, data: string) {.inline.} =
+  root:
+    existsOrCreateStorageDir(@["repos"])
+    writeFile(BASE_STORAGE / "repos" / name & ".argon", data)
 
 proc getPackageList*(
   name: string
